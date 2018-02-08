ad302d2e5f92ba6b02dc42e2d1480b623998b4cf
Andrey N <dluhhbiu@gmail.com>
2018-02-08 09:32:13 +0300

    feat: add procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+upgrade: alembic upgrade head
--- alembic/env.py
+++ alembic/env.py
@@ -13,6 +13,7 @@ import models
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option('sqlalchemy.url', os.environ['DATABASE_URL'])
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
